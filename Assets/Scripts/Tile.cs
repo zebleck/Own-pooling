b5255e8c4d85e5c068b7c228c9a203be0d35a10e
Fabian K <fabiankontor@aol.com>
2017-07-29 16:41:20 +0200

    Project added

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+
+    public RenderTile RenderTile;
+
+    public int n;
+
+    public Tile(int n)
+    {
+        this.n = n;
+    }
+}
---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap {
+
+    public int width;
+    public int height;
+    Tile[,] Tiles;
+
+    // Use this for initialization
+    public TileMap(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        Tiles = new Tile[width, height];
+        generate();
+    }
+
+    public void generate()
+    {
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                Tiles[x, y] = new Tile(y * width + x);
+                //GameObject GO = GameObject.Instantiate(Resources.Load<GameObject>("Tile"));
+                //GO.transform.position = new Vector2(x-width/2, y-height/2);
+                //GO.GetComponentInChildren<TextMesh>().text = (width * y + x).ToString();
+                //GO.SetActive(false);
+            }
+        }
+    }
+
+    public Tile getTile(int x, int y) {
+        return (x >= 0 && x < width && y >= 0 && y < height) ? Tiles[x, y] : null;
+    }
+
+
+}
